55a62def4f167f8191a13a83ee0a445d0540e378
Alexandre BERTRAND <lxndr.bertrand@gmail.com>
2017-09-14 12:21:19 +0200

    Update server.js
    Make Server Functionnal Again !

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const Discord = require("discord.js");
+// request peut etre une constante
 var request = require("request");
 const client = new Discord.Client();
 
@@ -8,19 +9,39 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === '!Classement') {
+  // Si tu veux verifier un élément qui est obligatoire pour toute la suite de ton code, tu peut écrire comme ça. 
+  // Ça t'évite de prendre un niveau d'indentation direct, et puisque tu est dans une fonction, si tu return, tu coupe le reste
+  if (msg.content !== '!Classement') 
+    return 
+    // options peut être une constante
     var options = {
                         method: 'GET',
+      // L'URL peut etre défini en soit en variable d'env, soit en constante 
                         url: 'http://api.football-data.org/v1/competitions/450/leagueTable',
-                        headers: { 'X-Auth-Token': '2459d8cb166a476a84cf82a8fa415b4d' }
+      // NE JAMAIS PUSH DE TOKEN SUR GITHUB !!!!! 
+      // Ici, je t'ai passé tes tokens en variable d'env. Ça te permet de rendre ton code utilisable pour n'importe qui
+      // Pour déclarer des variables d'env, soit tu les passe au démmarage (cf google), soit tu utilise un .env, soit tu export directement dans ton terminal
+                        headers: { 'X-Auth-Token': process.env.AUTH_TOKEN }
                   }
-
+// je pense que tu peut utiliser fetch (je suis plus sur)
                 request(options, function (error, response, body) {
-                if (error) throw new Error(error);
+                  
+                  // if error throw error est une pratique qu'on appelle "let it crash". Perso j'adore, par contre, 
+                  //il faut faire attention à ce que ton process node redemmare tout seul. (sinon, il va juste crasher)
+                  // d'ailleurs, sur le premier if, tu pourrait faire la mme chose
+                  if (error) throw new Error(error);
+                        // pas de console.log ... :p
                         console.log(body);
+                        // peut être une constante
                         var jsonData = JSON.parse(body)
                         var classement = "";
                         msg.reply("Le classement de ligue 1 :")
+                        // Alors, viens ici le plus intéressant ! Bon déja, arrow fn c'est mieux
+                        // ensuite, tu a deux solution pour que ça soit fonctionnel :
+                        // - Soit tu reduce ton array dans une string, ce qui fait que sur ton .reduce((prec, now), prec devient 
+                          // classement, et now element
+                        // Soit tu map ton tableau, et tu le join à la fin 
+                        // dans tout les cas, on kill la dernière variable mutable de ton code comme ça, et ça c'est cool ! 
                         jsonData.standing.forEach(function(element) {
                             classement += element.position + " : " + element.teamName + "   " + element.points + "\n";
                         });
@@ -30,4 +51,5 @@ client.on('message', msg => {
 
 });
 
-client.login('MzU2ODg3NTk5Mjc0NjU1NzY2.DJh5Cw.7Q8gkyXwPwTS4TbngV7RnqHWFCQ');
+// NE JAMAIS PUSH DE TOKEN SUR GITHUB !!!!! 
+client.login(process.env.TOKEN);
