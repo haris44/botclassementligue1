09a5269b7a3a6f90f97eddb06cd2cc73aeef838e
Netoun <coulonnier.nicolas.44@gmail.com>
2017-09-14 20:05:32 +0200

    Ajout expres.js

--- server.js
+++ server.js
@@ -2,8 +2,29 @@
 const Discord = require("discord.js");
 const request = require("request");
 const client = new Discord.Client();
+const express = require('express');
+const app = express();
 
 const urlLigue1 = 'http://api.football-data.org/v1/competitions/450/leagueTable';
+
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -34,3 +55,7 @@ client.on('message', msg => {
 
 // NE JAMAIS PUSH DE TOKEN SUR GITHUB !!!!!
 client.login(process.env.TOKEN);
+
+setInterval(() => {
+ http.get('https://polar-temple-67867.herokuapp.com/');
+}, 900000);
